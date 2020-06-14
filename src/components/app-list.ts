1fcc6188da833ce156ea622ab76b0312a678f820
ddiu8081 <ddiu8081@gmail.com>
2020-06-14 16:21:33 +0800

    feat: 地铁图

--- src/components/app-list.ts
+++ src/components/app-list.ts
@@ -1,7 +1,18 @@
 import { createComponent } from '@mpxjs/core'
+declare function requirePlugin<T>(name: string): T
 
 createComponent({
   data: {
-    listData: ['手机', '电视', '电脑']
+    listData: ['附近公交', '地铁图']
+  },
+  methods: {
+    handleTap1 (): void {
+      requirePlugin('subway')
+      const key = 'VRCBZ-TC56I-ZO3GO-5KFML-D4ZH2-2ABUW'
+      const referer = 'BusMet'
+      wx.navigateTo({
+        url: 'plugin://subway/index?key=' + key + '&referer=' + referer
+      })
+    }
   }
 })
